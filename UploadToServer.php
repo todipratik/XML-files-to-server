bc6c3600b3d72e9f1e9f21f74d209e1b45013c11
Pratik Todi <ptodi.y12@lnmiit.ac.in>
2015-06-23 11:14:05 +0530

    changes in script

--- UploadToServer.php
+++ UploadToServer.php
@@ -2,8 +2,33 @@
 
 	$file_path = "uploads/";
 
-	$file_path = $file_path . basename( $_FILES['uploaded_file']['name']);
+	$file_path = $file_path . basename($_FILES['uploaded_file']['name']);
 	if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $file_path)) {
+		// load the uploaded file
+		$file = "uploads/" . basename($_FILES['uploaded_file']['name']);
+
+		$doc_received = new DOMDocument('1.0');
+		$doc_received->preserveWhiteSpace = false;
+		$doc_received->load($file);
+		$doc_received->formatOutput = true;
+		// get the entry in the uploaded file
+		$node = $doc_received->getElementsByTagName('message')->item(0);
+
+		// load data.xml
+		$doc_final = new DOMDocument('1.0');
+		$doc_final->preserveWhiteSpace = false;
+		$doc_final->load("uploads/data.xml");
+		$doc_final->formatOutput = true;
+
+		// import the entry to new document
+		$node = $doc_final->importNode($node, true);
+
+		// get root and append the child node
+		$root = $doc_final->documentElement;
+		$root->appendChild($node);	
+
+		//save file
+		$doc_final->save("uploads/data.xml");
 		echo "success";
 	} else {
 		echo "fail";
